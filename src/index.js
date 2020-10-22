123c69f6deb01f2f495efb54d5385e1d572829e8
Bakin-dima <bakin.dima93@gmail.com>
2020-10-22 20:27:32 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = check = (str, bracketsConfig) => {
+    let ckeckArray = bracketsConfig.map((item) => item.join(""));
+    for (let i = 0; i < ckeckArray.length; i++) {
+        if (str.includes(ckeckArray[i])) {
+            str = str.replace(ckeckArray[i], "");
+            i = -1;
+        }
+    }
+    return str.length === 0;
+};
